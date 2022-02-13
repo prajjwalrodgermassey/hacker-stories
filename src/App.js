e82ec36033fdba6c7fa63703de97b63102a06691
rodgermassey <007rodgermassey@gmail.com>
2022-02-13 06:38:18 +0530

    working with map and lists

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const title = "react";
+const list = [
+             { title:"react",
+               url:"https://reactjs.org"
+             },
+             {
+               title:'redux',
+               url:"https://redux.js.org/"
+             }
+             ]
+
 function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <h2>welcome to the world of {title}</h2>
+      <label htmlFor = "search ">Search</label>
+      <input type = "text" id = "search" />
+      <hr/>
+
+      {list.map(item =>{
+            return <div> {item.title + "strong"}</div>;
+      })}
+
     </div>
   );
 }
