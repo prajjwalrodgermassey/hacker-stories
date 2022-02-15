2c36db04aca816bb95389cec9f500ebbb1772a35
rodgermassey <007rodgermassey@gmail.com>
2022-02-16 01:51:22 +0530

    arrow functions and event object and envet.target.value and handler functions

--- src/App.js
+++ src/App.js
@@ -27,12 +27,17 @@ const list = [
              ]
 
 function App() {
+
+  const handleChange = (event)=> 
+    console.log(event.target.value);             // this line will act as return statement. while using arrow functions we can omit return 
+                                    // return statement if the function contains single line.
+
   return (
     <div>
       <h1>Hello World!</h1>
       <h2>welcome to the world of {setTitle("reactjs")}</h2>
       <label htmlFor = "search ">Search</label>
-      <input type = "text" id = "search" />
+      <input type = "text" id = "search" onChange={handleChange}/>
       {/* <div>{outernotsofun()}</div> */}
       <hr/>
 
